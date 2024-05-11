cca33792b7b43f0c7d551bebab2a84f66de6aace
Ngoc Tan <ngoctan9102000@gmail.com>
2024-05-11 22:39:47 +0700

    create animal

--- app/src/main/java/com/training/myproject/MainActivity.kt
+++ app/src/main/java/com/training/myproject/MainActivity.kt
@@ -2,10 +2,47 @@ package com.training.myproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.d("Test", "onCreate")
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Test", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Test", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Test", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Test", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Test", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Test", "onDestroy")
+    }
+
 }
+
+
